ddcc1c3d7bfe051f06b8f0e79f5fc7ddef0b8da1
Leonardo S Salazar <steven.salazar583@gmail.com>
2022-11-23 15:27:31 -0800

    ...

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,22 @@
+document.write(' Hello, World!');
+
+var A = "This is a string|"
+document.write(A);
+
+var B = "This is a string |"
+window.alert(B);
+
+document.write(" Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded. |");
+
+document.write(" \"Be who you are and say what you feel,"
++ " because those who mind don't matter and those who matter don't mind.\""
++ "-Dr.Seuss |");
+
+var C = "Concatenated" + "String |"
+document.write(C);
+
+var Family = "The Titus\' |", Dad = "Robert", Mom = "Silvia", Son = "Steven"
+document.write(Family)
+
+var D = 3+3
+document.write(D)
