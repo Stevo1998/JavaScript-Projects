3f79938a475e6fdff1d14fcb4fe4a9dfa20b77a1
Leonardo S Salazar <steven.salazar583@gmail.com>
2022-11-23 18:45:11 -0800

    Added a repository
    Added a Project2_functions folder containing HTML and CSS files

---
+++ Project2_funtions/JS/main.js
@@ -0,0 +1,10 @@
+function MathProblem() {
+    var A = "Did you know that 5 multiplied by 5 is equal to ", B = 5*5
+    document.getElementById("Fact").innerHTML = A + B;
+}
+
+function SurpriseFunction() {
+    var sentence = "Have a very nice day";
+    sentence += " and Happy Thanks Giving!";
+    document.getElementById("Surprise").innerHTML = sentence;
+}
